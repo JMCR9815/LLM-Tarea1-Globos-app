9af01fb74ffae641b61db9c1a9f1387a7de9bc6b
JMCR9815 <38430152+JMCR9815@users.noreply.github.com>
2021-02-16 20:56:04 +0100

    Ejercicio Transiciones LLM

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let Boton=document.getElementById("Boton");
+Boton.onclick=globos;
+
+function globos(){
+
+	function eliminarIcono(elemento){
+		elemento.remove();
+	}
+
+	function InsertarIcono(){
+
+		Boton.classList.add("display");
+
+		const icono=document.createElement("div");
+
+		icono.classList.add("icono");
+
+		icono.innerHTML="🎈";
+
+		icono.style.left= Math.random()*100+"vw";
+
+		icono.style.animationDuration= Math.random()*2+3+"s";
+
+		document.body.appendChild(icono);
+
+		setTimeout(eliminarIcono,3000,icono);
+					
+	}
+
+	setInterval(InsertarIcono,1000);
+}
+
+
+
